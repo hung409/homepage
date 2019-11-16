57c66e7060e208fee07609d1426fbcbb721778c9
shyuniz <shyuniz@naver.com>
2019-11-16 17:41:42 +0900

    vue test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import './test.js';
+import './js/test.js';
+//import 'https://cdn.jsdelivr.net/npm/vue/dist/vue.js';
+
+//<!-- 개발버전, 도움되는 콘솔 경고를 포함. -->
+//<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+//<!-- 상용버전, 속도와 용량이 최적화됨. -->
+//<script src="https://cdn.jsdelivr.net/npm/vue"></script>
 
 function writecom() {
   let element = document.createElement('div');
@@ -6,3 +12,13 @@ function writecom() {
   return element;
 }
 document.body.appendChild(writecom());
+
+
+
+
+import Vue from 'vue'
+import AppComponent from './vue_component/AppComponent.vue'
+
+new Vue({
+  render: h => h(AppComponent)
+}).$mount('#app')
---
+++ src/vue_component/AppComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <pre>
+      <h1>{{ greeting }} from the AppComponent</h1>
+      OtherComponent OtherComponent
+    </pre>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        greeting: 'HELLO'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
